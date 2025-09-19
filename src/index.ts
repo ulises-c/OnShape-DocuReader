2aabce6d819d5e7ec0869d2f13238bcb707cbd1c
Ulises Chavarria <ulises@lazamedical.com>
2025-09-19 16:00:22 -0700

    updated 404 handler

--- src/index.ts
+++ src/index.ts
@@ -72,8 +72,8 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
   });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
+// 404 handler - catch all unmatched routes
+app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
