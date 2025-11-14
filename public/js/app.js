14e2275a9e9f8688e16d239d3bee40e1655c4abf
Ulises Chavarria <ulises@lazamedical.com>
2025-11-14 15:07:30 -0800

    Revert "Reapply "folder grouping""
    This reverts commit 12e6c83f16666e2e22af798c20bcbb24f9aae7dc.

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ import { AuthService } from "./services/auth-service.js";
 import { DocumentService } from "./services/document-service.js";
 import { ExportService } from "./services/export-service.js";
 import { ThumbnailService } from "./services/thumbnail-service.js";
-import { FolderService } from "./services/folder-service.js";
 import { Navigation } from "./views/navigation.js";
 import { ModalManager } from "./views/modal-manager.js";
 import { AppController } from "./controllers/app-controller.js";
@@ -19,13 +18,11 @@ const state = new AppState();
 const apiClient = new ApiClient();
 
 // Services bundle
-const folderService = new FolderService();
 const services = {
   authService: new AuthService(apiClient),
-  documentService: new DocumentService(apiClient, folderService),
+  documentService: new DocumentService(apiClient),
   exportService: new ExportService(apiClient),
   thumbnailService: new ThumbnailService(),
-  folderService,
   apiClient: apiClient,
 };
 
